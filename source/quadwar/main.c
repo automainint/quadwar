d04b9c9c7d6e7d7bd94e4c23f050694e20af53e6
Mitya Selivanov <automainint@guattari.tech>
2022-09-22 21:50:25 +0400

    test

--- source/quadwar/main.c
+++ source/quadwar/main.c
@@ -37,9 +37,9 @@ int main(int argc, char **argv) {
 
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                       SDL_GL_CONTEXT_PROFILE_ES);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, gl_major);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, gl_minor);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 
   SDL_Window *const window = SDL_CreateWindow(
       "Quadwar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
